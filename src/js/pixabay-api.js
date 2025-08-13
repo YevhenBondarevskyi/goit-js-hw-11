74291e0c93e8d25f83f597cf64892ac1bbddad45
Yevhen <bondarevskiy@gmail.com>
2025-08-13 21:23:13 +0200

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export function getImagesByQuery(query) {
-    const API_KEY = ;
+    const API_KEY = '51053325-3e2bfd05922f90ae0c618f7f8';
 
     const params = new URLSearchParams({
         key: API_KEY,
